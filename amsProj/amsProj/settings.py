93457748629846115a756275edd534740cdbdbd6
bvsalangsang <bobby.salangsang@gmail.com>
2025-05-08 10:21:10 +0800

    updated main route to dashboard

--- amsProj/amsProj/settings.py
+++ amsProj/amsProj/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    #for local 
+    # for local 
     #  "default": {
     #     "ENGINE": 'django.db.backends.mysql',
     #     "NAME": 'ams',
